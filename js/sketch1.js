07efa6539f52781eefe2b4d913bab351c8e4ed43
Grant Barland <35273219+gbarland@users.noreply.github.com>
2022-07-06 15:38:00 -0500

    7/6 sketch

--- js/sketch1.js
+++ js/sketch1.js
@@ -1,19 +1,17 @@
-//disable scrolling on iOS
-document.addEventListener('touchstart', function (e) {
-    e.preventDefault();
-  });
   
   function smoothstep(edge0, edge1, x) {
     t = constrain((x - edge0)/(edge1 - edge0), 0.0, 1.0);
     return t * t * (3.0 - 2.0 * t);
   }
   
+  let noiseScale=0.02;
+
   function draw() {
     background(41,42,45);
     translate(width/2,height/2);
     steps = min(width, height)/4.5;
     stroke(194, 69, 108);
-    strokeWeight(2);
+    strokeWeight(1);
     noFill();
     let mx = (mouseX || width/2)/width*1000;
     let my = (mouseY || height/2)/height*1000;
@@ -23,19 +21,18 @@ document.addEventListener('touchstart', function (e) {
       let radius = smoothstep(0, steps/2.5, i) * height;
       let x = sin(frameCount*i/mx)*(radius);
       let y = cos(frameCount*i/my)*(radius);
-      let z = cos(frameCount*i/1000.0)*(radius/10.0);
-      curveVertex(x, y, z);
+      let z = cos(frameCount * i / 1000.0) * (radius / 10.0);
+      let noiseVal = noise(mouseX*mouseY*noiseScale);
+      curveVertex(x+noiseVal, y+noiseVal, z+noiseVal);
     }
     endShape();
   }
   
 
 
-
 function setup() {
-    var canvas = createCanvas(windowWidth/2, windowWidth/2);
-
-    canvas.parent('sketch-holder');
+  var canvas = createCanvas(500, 500);
+  canvas.parent('sketch-holder');
 }
 
   
